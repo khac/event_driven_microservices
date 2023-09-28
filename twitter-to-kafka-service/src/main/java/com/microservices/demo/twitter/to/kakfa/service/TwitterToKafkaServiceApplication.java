d9e2968c63c88a3ae05b041ab33817c8cc3f6af9
khac <aditxy@hotmail.com>
2023-09-27 21:35:58 -0700

    fix: versions

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kakfa/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kakfa/service/TwitterToKafkaServiceApplication.java
@@ -1,11 +1,17 @@
 package com.microservices.demo.twitter.to.kakfa.service;
 import com.microservices.demo.twitter.to.kakfa.service.config.TwitterToKafkaServiceConfigData;
+import com.microservices.demo.twitter.to.kakfa.service.runner.StreamRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
+// Dependency Injection with Construction Injection
+//https://www.youtube.com/watch?v=aX-bgylmprA
 @SpringBootApplication
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
@@ -13,15 +19,21 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
     private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
 
-    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData) {
+    private final StreamRunner streamRunner;
+    @Autowired
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData, StreamRunner runner) {
         this.twitterToKafkaServiceConfigData = configData;
+        this.streamRunner = runner;
     }
-    public static void main(String[] args) {SpringApplication.run(TwitterToKafkaServiceApplication.class, args);}
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
 
     @Override
     public void run(String... args) throws Exception {
         LOG.info("App starts... ");
-        LOG.info(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {}));
+        LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
         LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
+        streamRunner.start();
     }
 }
